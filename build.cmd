dd4e4ba3fe485f90e5c07cdb3bbe84430406d00d
Julian Easterling <julian@julianscorner.com>
2022-07-31 02:06:41 -0400

    updated build scripts with split version

--- build.cmd
+++ build.cmd
@@ -3,12 +3,13 @@ setlocal
 
 pushd %~dp0
 
-for /f "delims=" %%x in (version) do set VERSION=%%x
+for /f "delims=" %%x in (VERSION) do set VERSION=%%x
+for /f "delims=" %%x in (REVISION) do set REVISION=%%x
 
-docker build --progress plain -t dcjulian29/nmap:%VERSION% .
+docker build --progress plain --build-arg VERSION=%VERSION% -t dcjulian29/nmap:%VERSION%.%REVISION% .
 
 if %errorlevel% neq 0 popd;exit /b %errorlevel%
 
 popd
 
-docker tag dcjulian29/nmap:%VERSION%  dcjulian29/nmap:latest
+docker tag dcjulian29/nmap:%VERSION%.%REVISION%  dcjulian29/nmap:latest
